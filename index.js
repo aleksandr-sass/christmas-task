7b4ebf4a2005faa3206c7bc2e9fb984c765bae53
Aleksandr Sass <a.sass@tut.by>
2022-05-29 16:59:01 +0300

    feat: set up the view for 60 toys

--- index.js
+++ index.js
@@ -1,31 +1,52 @@
-const toy = document.querySelector("#toy");
-const obj = data[0];
+initDivMaker(data);
+dataToDivGrid(data)
 
-const toyHeaderHTML = `<h3>${obj.name}</h3>`;
-const toyFlexContainerHTML = `<div id="flex-container"></div>`;
-const toyImageHTML = `<div id="img"><img src="./assets/toys/${obj.num}.png" alt="${obj.name}"></div>`;
-let toyTextHTML = '';
+function dataToDivGrid(array) {
+  for (let i=1; i<=array.length; i +=1) {
+    makeTheToy(array[i-1], i);
+  }
+}
 
-const toyCountHTML = `<p>Количество: ${obj.count}</p>`;
-const toyYearOfPurchaseHTML = `<p>Год покупки: ${obj.year}</p>`;
-const toyShapeHTML = `<p>Форма: ${obj.shape}</p>`;
-const toyColorHTML = `<p>Цвет: ${obj.color}</p>`;
-const toySizeHTML = `<p>Размер: ${obj.size}</p>`;
-const toyIsFavoriteHTML = `<p>Любимая: ${boolToRussian(obj.favorite)}</p>`;
+function initDivMaker(array) {
+  let text = '';
+  let length = array.length;
+  for (let i=1; i<=length; i += 1) {
+    text += `<div id="toy${i}" class="toy"></div>`;
+  };
+  const grid = document.querySelector('#grid');
+  grid.innerHTML = text;
+}
 
-toyTextHTML = `<div id="toy-text">` +
-             toyCountHTML + 
-             toyYearOfPurchaseHTML +
-             toyShapeHTML +
-             toyColorHTML +
-             toySizeHTML +
-             toyIsFavoriteHTML + `</div>`;
+function makeTheToy(obj, index) {
+  const toy = document.querySelector(`#toy${index}`);
 
-toy.innerHTML = toyHeaderHTML + toyFlexContainerHTML;
+  const toyHeaderHTML = `<h3>${obj.name}</h3>`;
+  const toyFlexContainerHTML = `<div class="flex-container"></div>`;
+  const toyImageHTML = `<div class="img"><img src="./assets/toys/${obj.num}.png" alt="${obj.name}"></div>`;
+  let toyTextHTML = '';
 
-const toyFlexContainer = document.querySelector("#flex-container");
-toyFlexContainer.innerHTML = toyImageHTML + toyTextHTML;
+  const toyCountHTML = `<p>Количество: ${obj.count}</p>`;
+  const toyYearOfPurchaseHTML = `<p>Год покупки: ${obj.year}</p>`;
+  const toyShapeHTML = `<p>Форма: ${obj.shape}</p>`;
+  const toyColorHTML = `<p>Цвет: ${obj.color}</p>`;
+  const toySizeHTML = `<p>Размер: ${obj.size}</p>`;
+  const toyIsFavoriteHTML = `<p>Любимая: ${boolToRussian(obj.favorite)}</p>`;
 
+  toyTextHTML = `<div id="toy-text">` +
+               toyCountHTML + 
+               toyYearOfPurchaseHTML +
+               toyShapeHTML +
+               toyColorHTML +
+               toySizeHTML +
+               toyIsFavoriteHTML + `</div>`;
+
+  toy.innerHTML = toyHeaderHTML + toyFlexContainerHTML;
+
+  const toyFlexContainer = document.querySelectorAll(".flex-container")[index-1];
+  toyFlexContainer.innerHTML = toyImageHTML + toyTextHTML;
+}
+
+/* Some helpful JavaScript functions */
 
 function boolToRussian(value) {
   if (value) return `да`;
